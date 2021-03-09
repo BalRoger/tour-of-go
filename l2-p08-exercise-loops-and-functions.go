5da664a8dc3c57a153ea374856f23d9e2ad95902
Roger Glover <roger.glover@optum.com>
2021-03-09 10:13:46 -0600

    completed exercise

--- l2-p08-exercise-loops-and-functions.go
+++ l2-p08-exercise-loops-and-functions.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func Sqrt(x float64) float64 {
+	e := 0.00000000000000025
+	zold, z :=0.0, 1.0
+	for z-zold >= e || z-zold <= -e {
+		zold = z
+		z -= (z*z - x) / (2*z)
+	}
+	return z
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(2), math.Sqrt(2))
 }
