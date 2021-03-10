1bf595654c781a7a3dc9b544e4a5ed9ad424c3e0
Roger Glover <roger.glover@optum.com>
2021-03-09 20:19:45 -0600

    After Errors exercise.

--- l4-p20-exercise-errors.go
+++ l4-p20-exercise-errors.go
@@ -5,11 +5,9 @@ Copy your Sqrt function from the earlier exercise and modify it to return an err
 Sqrt should return a non-nil error value when given a negative number, as it doesn't support complex numbers.
 
 Create a new type
-
-type ErrNegativeSqrt float64
+    type ErrNegativeSqrt float64
 and make it an error by giving it a
-
-func (e ErrNegativeSqrt) Error() string
+    func (e ErrNegativeSqrt) Error() string
 method such that ErrNegativeSqrt(-2).Error() returns "cannot Sqrt negative number: -2".
 
 Note: A call to fmt.Sprint(e) inside the Error method will send the program into an infinite loop. You can avoid this
@@ -24,13 +22,27 @@ import (
     "fmt"
 )
 
-func Sqrt(x float64) (float64, error) {
-    return 0, nil
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Squirt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+
+	epsilon := 0.00000000000000025
+	zold, z :=0.0, 1.0
+	for z-zold >= epsilon || z-zold <= -epsilon {
+		zold = z
+		z -= (z*z - x) / (2*z)
+	}
+	return z, nil
 }
 
 func main() {
-    fmt.Println(Sqrt(2))
-    fmt.Println(Sqrt(-2))
+    fmt.Println(Squirt(2))
+    fmt.Println(Squirt(-2))
 }
-
- */
